23a748d53366320d7318e8ee1dc515fa94aaeeb9
= <andy.block@gmail.com>
2012-07-21 13:37:20 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yourdoor::Application.config.secret_token = '4ce79ab4f7172db97a68602042e22e460840aa011e45b5d0fb00473147e471fcccf3f1cf4862865ae5ac8ef01fcd9f1c60950f21547e212f05586b0500d573d3'
